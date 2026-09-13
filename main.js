53e4b2258e2e226dffbd3eb50d929fec56096d47
mkr_pics <mickaelreignier974@gmail.com>
2026-09-14 01:01:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
    la vignette prend le cadrage portrait standard. */
 const PORTFOLIO_DATA = [
   { cat: "sport", label:"1", src: "walid.JPEG", pos: "center 20%" },
-  { cat: "sport", label:"2", src: "mel.JPEG", pos: "center 20%" },
+  { cat: "sport", label:"2", src: "melv.JPEG", pos: "center 20%" },
   { cat: "sport", label:"3", src: "sport-01.jpg", pos: "center 20%" },
   { cat: "sport", label:"4", src: "sport-02.jpg", pos: "center 15%" },
   { cat: "sport", label:"5", src: "sport-03.jpg", pos: "center 15%" },
